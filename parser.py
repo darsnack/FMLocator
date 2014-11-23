71db59ac87c8b36a7df85f36f58413f14dfb7362
unknown <raosg@RAOSG-1.rose-hulman.edu>
2014-11-23 11:47:33 -0600

    Fuck base 26

--- parser.py
+++ parser.py
@@ -1,4 +1,30 @@
+from string import ascii_uppercase
 
+def piToCall(piCode):
+    piDeci = int(piCode,16)
+    callsign = ""
+    digits = str(piDeci)
+    if piDeci >= 21672:
+        callsign+="W"
+        inductor = piDeci - 21762
+        print(int_to_b26(inductor))
+        callsign+=int_to_b26(inductor)
+
+    else:
+        callsign+="K"
+        inductor = piDeci - 4096
+        callsign+=base10toN(int(piCode[1]),26)
+        callsign+=base10toN(int(piCode[2]),26)
+        callsign+=base10toN(int(piCode[3]),26)
+    return callsign
+
+def int_to_b26(n):
+    if n < 25:
+        return ascii_uppercase[n]
+    else:
+        q, r = divmod(n, 25)
+        return int_to_b26(q) + ascii_uppercase[r]
+    
 
 def data_parser():
 
@@ -14,9 +40,5 @@ def data_parser():
 
    
 if __name__=="__main__":
-    result=data_parser()
-    print('Longitude:',str(result['Longitude']))
-    print('Latitude:',str(result['Latitude'])+'\n')
-    print('Callsign:',str(result['Callsign']))
-    while True: continue
-
+    callsign = piToCall("9331")
+    print(callsign)
