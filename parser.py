f2f4177b9710d0c269b521e2a001e1e7820eaa6a
Tony <milluza1@rose-hulman.edu>
2014-11-23 01:21:04 -0500

    fuck kyle again

--- parser.py
+++ parser.py
@@ -1,17 +1,22 @@
-import csv
 
-info={'Longitude': [], 'Latitude': [], 'Callsign':[]}
 
-with open('fm_stations.csv') as fobj:
-    next(fobj)
-    for line in fobj:
-        rawdata = line.split(',')
-        info['Longitude'].append(rawdata[0])
-        info['Latitude'].append(rawdata[1])
-        info['Callsign'].append(rawdata[2].split('-',1)[0])
+def data_parser():
 
-#print('Longitude:',str(info['Longitude']))
-#print('Latitude:',str(info['Latitude'])+'\n')
-#print('Callsign:', str(info['Callsign']))
-while True: continue
+    info={'Longitude': [], 'Latitude': [], 'Callsign':[]}
+    with open('fm_stations.csv') as fobj:
+        next(fobj)
+        for line in fobj:
+            rawdata = line.split(',')
+            info['Longitude'].append(rawdata[0])
+            info['Latitude'].append(rawdata[1])
+            info['Callsign'].append(rawdata[2].split('-',1)[0])
+    return info
+
+   
+if __name__=="__main__":
+    result=data_parser()
+    print('Longitude:',str(result['Longitude']))
+    print('Latitude:',str(result['Latitude'])+'\n')
+    print('Callsign:',str(result['Callsign']))
+    while True: continue
 
